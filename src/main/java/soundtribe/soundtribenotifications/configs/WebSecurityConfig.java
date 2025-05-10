9f241c33977dafdaf4bb838e8f6bdfba13ab4afb
Garbi-Collector <uwurulos@gmail.com>
2025-05-10 14:10:15 -0300

    cambio de seguridad, se permiten los patchs

--- src/main/java/soundtribe/soundtribenotifications/configs/WebSecurityConfig.java
+++ src/main/java/soundtribe/soundtribenotifications/configs/WebSecurityConfig.java
@@ -32,7 +32,7 @@ public class WebSecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(frontUrl));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
